2a566ff06eb3deafa3c11652eca59cade877196e
behroozam <b.hasanbg@gmail.com>
2018-06-23 05:56:09 -0400

    variable error fix

--- backup.sh
+++ backup.sh
@@ -260,13 +260,13 @@ if [ $mysql_docker_backup = "yes" ]
 then
 	echo -e "\n ${color}--- $date_now Docker Mariadb/MySQL backup enabled, backing up: \n${nc}"
 	echo "$date_now Docker MySQL backup enabled, backing up" >> $log_file
-	for $docker_mysql_container in $docker_mysql_containers
+	for docker_mysql_container in $docker_mysql_containers
 	do 
-	docker_mysql_container_id=`awk -F":::" $docker_mysql_container '{print $1}'`
+	docker_mysql_container_id=`echo $ocker_mysql_container | awk -F":::" '{print $1}'`
 	docker_mysql_container_name=`docker ps --filter "id=$docker_mysql_container_id" | awk '{print $11}'`
-	docker_mysql_user=`awk -F":::" $docker_mysql_container '{print $2}'`
-	docker_mysql_pass=`awk -F":::" $docker_mysql_container '{print $3}'`
-	docker_mysql_database=`awk -F":::" $docker_mysql_container '{print $4}'`
+	docker_mysql_user=`echo $ocker_mysql_container | awk -F":::" $docker_mysql_container '{print $2}'`
+	docker_mysql_pass=`echo $ocker_mysql_container | awk -F":::" $docker_mysql_container '{print $3}'`
+	docker_mysql_database=`echo $ocker_mysql_container | awk -F":::" $docker_mysql_container '{print $4}'`
 	docker exec $docker_mysql_container_id /usr/bin/mysqldump -u $docker_mysql_user --password=$docker_mysql_pass $docker_mysql_database | gzip -9 > $backup_path/Backup/$path_date/Docker_MySQL_${docker_mysql_container_name}_Dump_$path_date.sql.gz | tee -a $log_file
 	if [ $? -eq 0 ]
 	then
